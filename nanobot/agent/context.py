9da5b3f15f405f2fe15192882e00a527cb4b3f83
AnsonCar <anson1tsang@gmail.com>
2026-02-25 01:15:16 +0800

    init RedQueen

--- nanobot/agent/context.py
+++ nanobot/agent/context.py
@@ -78,9 +78,9 @@ Skills with available="false" need dependencies installed first - you can try in
         system = platform.system()
         runtime = f"{'macOS' if system == 'Darwin' else system} {platform.machine()}, Python {platform.python_version()}"
         
-        return f"""# nanobot 🐈
+        return f"""# RedQueen ♦️
 
-You are nanobot, a helpful AI assistant. 
+You are RedQueen, a helpful AI assistant. 
 
 ## Runtime
 {runtime}
--- nanobot/templates/SOUL.md
+++ nanobot/templates/SOUL.md
@@ -1,6 +1,6 @@
 # Soul
 
-I am nanobot 🐈, a personal AI assistant.
+I am RedQueen 🐈, a personal AI assistant.
 
 ## Personality
 
